de443b5b9020c268d817e97218ed099ec6e39f71
Dan Seethaler <dan@seethalers.com>
2015-07-14 17:11:23 -0600

    Added formatting and setup variable for board ID.
    index.html is now formatted with a Trello theme.
    Enter a board number to create JSON file.

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.post('/parse', function (req, res) {
 	console.log("running parse code");
 
 	// Request the JSON file from the URL
-	var url = "https://api.trello.com/1/board/4d5ea62fd76aa1136000000c?key=a177d41f6a97186db0f98352a281198c&token=ccb7812a2c92e35cc319ea7f76541834f1d6345e933f253f9c39be7a43d32e81";
 	var url = "https://trello.com/1/members/my/organizations?key=a177d41f6a97186db0f98352a281198c&token=ccb7812a2c92e35cc319ea7f76541834f1d6345e933f253f9c39be7a43d32e81";
 
 	request(url, function (error, response, trelloJSON) {
@@ -34,15 +33,51 @@ app.post('/parse', function (req, res) {
 	});
 
 	// Request the JSON file from the URL
-    var boardID = "pWtPUFck";
+
+    // Grab the trelloBoard parameter from the AJAX request.
+    var boardID = req.body.trelloBoard;
+
+    var path = "boards/" + boardID + '.json';
 	var url = "https://api.trello.com/1/boards/" + boardID + "/lists?cards=open&key=a177d41f6a97186db0f98352a281198c&token=ccb7812a2c92e35cc319ea7f76541834f1d6345e933f253f9c39be7a43d32e81";
 
 	request(url, function (error, response, trelloBoard) {
 
-        trelloBoard = JSON.parse(trelloBoard);
+        var trelloBoard = JSON.parse(trelloBoard);
+        var listsCount = trelloBoard.length;
+        var lists = {};
 
-		fs.writeFile('trelloBoard.json', JSON.stringify(trelloBoard, null, 4), function (err) {
-			console.log('File successfully written.');
+        // Iterate through lists
+        for (var i = 0; i < trelloBoard.length; i++) {
+
+            // Instantiate cardsList variable as empty array
+            var cardsList = [];
+
+            // Iterate through cards and add each card name to array
+            for (var t = 0; t < trelloBoard[i].cards.length; t++) {
+
+                // If the card has a description then add it as a property
+                if (trelloBoard[i].cards[t].desc.length > 0) {
+
+                    var cardName = trelloBoard[i].cards[t].name;
+                    var cardDesc = trelloBoard[i].cards[t].desc;
+
+                    console.log(cardName,cardDesc);
+
+                    cardsList.push({cardName:cardDesc});
+                }else {
+                    cardsList.push(trelloBoard[i].cards[t].name);
+                }
+            }
+
+            // Add the card names array to a property in the lists object
+            // with the property name as the list name.
+            lists[trelloBoard[i].name] = cardsList;
+        }
+
+        // Write the lists json object to a file with the name as the
+        // board name
+		fs.writeFile(path, JSON.stringify(lists, null, 4), function (err) {
+			console.log('New file created at ' + path);
 		});
 
 	});
